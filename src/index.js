f217619cc855463d4c06d56e0936e2365a50724a
Aryan Agarwal <aryankush025@gmail.com>
2020-04-27 00:51:45 +0530

    task endpoint added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +13,19 @@ app.post('/user', (req, res) => {
 
 	user.save()
 		.then((currentUser) => {
-			res.send(currentUser)
+			res.status(201).send(currentUser)
+		})
+		.catch((error) => {
+			res.status(400).send(error)
+		})
+})
+
+app.post('/tasks', (req, res) => {
+	const task = new Task(req.body)
+
+	task.save()
+		.then((currentTask) => {
+			res.status(201).send(currentTask)
 		})
 		.catch((error) => {
 			res.status(400).send(error)
