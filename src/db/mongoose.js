c39905e5f5cdaf33d5e49fd331bf5b05ce4c8750
Aryan Agarwal <aryankush025@gmail.com>
2020-04-26 22:31:28 +0530

    validations added for user and tasks added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const R = require('ramda')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-api'
@@ -10,66 +11,81 @@ mongoose.connect(connectionURL + '/' + databaseName, {
 	useUnifiedTopology: true
 })
 
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is invalid')
-			}
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be greater than equal to 0.')
-			}
-		}
-	}
-})
-
-const me = new User({
-	name: '     Aryan Agarwal   ',
-	email: 'aryan@GEEKYANTS.com       '
-})
-
-me.save()
-	.then((user) => {
-		console.log(user)
-	})
-	.catch((error) => {
-		console.error(error)
-	})
-
-// const Task = mongoose.model('Task', {
-// 	description: {
-// 		type: String
+// const User = mongoose.model('User', {
+// 	name: {
+// 		type: String,
+// 		required: true,
+// 		trim: true
 // 	},
-// 	completed: {
-// 		type: Boolean
+// 	email: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 		lowercase: true,
+// 		validate(value) {
+// 			if (!validator.isEmail(value)) {
+// 				throw new Error('Email is invalid')
+// 			}
+// 		}
+// 	},
+// 	age: {
+// 		type: Number,
+// 		default: 0,
+// 		validate(value) {
+// 			if (value < 0) {
+// 				throw new Error('Age must be greater than equal to 0.')
+// 			}
+// 		}
+// 	},
+// 	password: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 		minlength: 7,
+// 		validate(value) {
+// 			if (R.includes('password', R.toLower(value))) {
+// 				throw new Error("Password must not contain string 'password'")
+// 			}
+// 		}
 // 	}
 // })
 
-// const task1 = new Task({
-// 	description: 'I have to complete this course by 27th April 2020',
-// 	completed: false
+// const me = new User({
+// 	name: '     Aryan Agarwal   ',
+// 	email: 'aryan@GEEKYANTS.com       ',
+// 	password: 'PASSWORD123'
 // })
 
-// task1
-// 	.save()
-// 	.then((task) => {
-// 		console.log(task)
+// me.save()
+// 	.then((user) => {
+// 		console.log(user)
 // 	})
 // 	.catch((error) => {
 // 		console.error(error)
 // 	})
+
+const Task = mongoose.model('Task', {
+	description: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	completed: {
+		type: Boolean,
+		default: false
+	}
+})
+
+const task1 = new Task({
+	description: 'I am DON          ',
+	completed: true
+})
+
+task1
+	.save()
+	.then((task) => {
+		console.log(task)
+	})
+	.catch((error) => {
+		console.error(error)
+	})
