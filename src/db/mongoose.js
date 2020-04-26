6b50e0c421ac490318873fd8e1f3942111b108d9
Aryan Agarwal <aryankush025@gmail.com>
2020-04-27 00:42:56 +0530

    user file made and route for /user added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
-const R = require('ramda')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-api'
@@ -10,59 +8,6 @@ mongoose.connect(connectionURL + '/' + databaseName, {
 	useCreateIndex: true,
 	useUnifiedTopology: true
 })
-
-// const User = mongoose.model('User', {
-// 	name: {
-// 		type: String,
-// 		required: true,
-// 		trim: true
-// 	},
-// 	email: {
-// 		type: String,
-// 		required: true,
-// 		trim: true,
-// 		lowercase: true,
-// 		validate(value) {
-// 			if (!validator.isEmail(value)) {
-// 				throw new Error('Email is invalid')
-// 			}
-// 		}
-// 	},
-// 	age: {
-// 		type: Number,
-// 		default: 0,
-// 		validate(value) {
-// 			if (value < 0) {
-// 				throw new Error('Age must be greater than equal to 0.')
-// 			}
-// 		}
-// 	},
-// 	password: {
-// 		type: String,
-// 		required: true,
-// 		trim: true,
-// 		minlength: 7,
-// 		validate(value) {
-// 			if (R.includes('password', R.toLower(value))) {
-// 				throw new Error("Password must not contain string 'password'")
-// 			}
-// 		}
-// 	}
-// })
-
-// const me = new User({
-// 	name: '     Aryan Agarwal   ',
-// 	email: 'aryan@GEEKYANTS.com       ',
-// 	password: 'PASSWORD123'
-// })
-
-// me.save()
-// 	.then((user) => {
-// 		console.log(user)
-// 	})
-// 	.catch((error) => {
-// 		console.error(error)
-// 	})
 
 const Task = mongoose.model('Task', {
 	description: {
@@ -75,17 +20,3 @@ const Task = mongoose.model('Task', {
 		default: false
 	}
 })
-
-const task1 = new Task({
-	description: 'I am DON          ',
-	completed: true
-})
-
-task1
-	.save()
-	.then((task) => {
-		console.log(task)
-	})
-	.catch((error) => {
-		console.error(error)
-	})
---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const R = require('ramda')
+
+const User = mongoose.model('User', {
+	name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Email is invalid')
+			}
+		}
+	},
+	age: {
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be greater than equal to 0.')
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 7,
+		validate(value) {
+			if (R.includes('password', R.toLower(value))) {
+				throw new Error("Password must not contain string 'password'")
+			}
+		}
+	}
+})
+
+module.exports = User
