de9a91f7d45d0b58a01b6a8aff7c3454df6fddd2
Aryan Agarwal <aryankush025@gmail.com>
2020-04-26 18:20:47 +0530

    create code removed

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,7 @@
-const { MongoClient, ObjectID } = require('mongodb')
+const { MongoClient } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-
-const id = new ObjectID()
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
 	if (error) {
@@ -13,67 +11,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 	console.info('Connected to DB')
 
 	const db = client.db(databaseName)
-
-	db.collection('users').insertOne(
-		{
-			_id: id,
-			name: 'Ikka',
-			age: 31
-		},
-		(error, result) => {
-			if (error) {
-				return console.error(error)
-			}
-
-			console.info(result.ops)
-		}
-	)
-
-	// db.collection('users').insertMany(
-	// 	[
-	// 		{
-	// 			name: 'Yo Yo Honey Singh',
-	// 			age: 31
-	// 		},
-	// 		{
-	// 			name: 'Badshah',
-	// 			age: 30
-	// 		},
-	// 		{
-	// 			name: 'Raftaar',
-	// 			age: 29
-	// 		}
-	// 	],
-	// 	(error, result) => {
-	// 		if (error) {
-	// 			return console.error(error)
-	// 		}
-
-	// 		console.info(result.ops)
-	// 	}
-	// )
-
-	// db.collection('tasks').insertMany(
-	// 	[
-	// 		{
-	// 			description: 'Clean the house',
-	// 			completed: true
-	// 		},
-	// 		{
-	// 			description: 'Renew inspection',
-	// 			completed: false
-	// 		},
-	// 		{
-	// 			description: 'Pot plants',
-	// 			completed: false
-	// 		}
-	// 	],
-	// 	(error, result) => {
-	// 		if (error) {
-	// 			return console.error(error)
-	// 		}
-
-	// 		console.info(result.ops)
-	// 	}
-	// )
 })
