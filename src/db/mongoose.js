732014a997219b2f2975efc22dd9fade780d9a51
Aryan Agarwal <aryankush025@gmail.com>
2020-04-26 21:45:34 +0530

    mongoose added

---
+++ src/db/mongoose.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager-api'
+
+mongoose.connect(connectionURL + '/' + databaseName, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+})
+
+const User = mongoose.model('User', {
+	name: {
+		type: String
+	},
+	age: {
+		type: Number
+	}
+})
+
+const me = new User({
+	name: 'Aryan',
+	age: 'jhadsb'
+})
+
+me.save()
+	.then((user) => {
+		console.log(user)
+	})
+	.catch((error) => {
+		console.error(error)
+	})
