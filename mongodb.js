36c7400fa21a39d01140746e31400602c2c49446
Aryan Agarwal <aryankush025@gmail.com>
2020-04-26 19:26:48 +0530

    read code added

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -11,4 +11,31 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 	console.info('Connected to DB')
 
 	const db = client.db(databaseName)
+
+	// db.collection('users').findOne({ _id: new ObjectID('5ea581a62dc2dc578fa26de1') }, (error, user) => {
+	// 	if (error) {
+	// 		return console.error('Error - ', error)
+	// 	}
+
+	// 	console.log('User', user)
+	// })
+
+	// db.collection('users')
+	// 	.find({ age: 31 })
+	// 	.toArray((error, users) => {
+	// 		if (error) {
+	// 			return console.error('Error - ', error)
+	// 		}
+
+	// 		console.log('users', users)
+	// 	})
+
+	db.collection('tasks')
+		.find({ completed: true })
+		.toArray((error, tasks) => {
+			if (error) {
+				return console.error('Error - ', error)
+			}
+			console.log('tasks', tasks)
+		})
 })
