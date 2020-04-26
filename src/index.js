cee67483f68d9e991129da3bd32182d4b2446b75
Aryan Agarwal <aryankush025@gmail.com>
2020-04-27 03:16:44 +0530

    new filr for user router added

--- src/index.js
+++ src/index.js
@@ -1,45 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-	const user = new User(req.body)
-
-	try {
-		await user.save()
-		res.status(201).send(user)
-	} catch (error) {
-		res.status(400).send(error)
-	}
-})
-
-app.get('/users', async (req, res) => {
-	try {
-		const users = await User.find({})
-		res.send(users)
-	} catch (error) {
-		res.status(400).send(error)
-	}
-})
-
-app.get('/users/:id', async (req, res) => {
-	try {
-		const user = await User.findById(req.params.id)
-		if (!user) {
-			return res.status(404).send()
-		}
-
-		res.send(user)
-	} catch (error) {
-		res.status(400).send(error)
-	}
-})
+app.use(userRouter)
 
 app.get('/tasks', (req, res) => {
 	Task.find({})
@@ -75,6 +43,45 @@ app.post('/tasks', (req, res) => {
 		.catch((error) => {
 			res.status(500).send(error)
 		})
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+	const updates = Object.keys(req.body)
+	const allowedUpdates = ['description', 'completed']
+
+	const isAllowed = updates.every((update) => allowedUpdates.includes(update))
+
+	if (!isAllowed) {
+		return res.status(400).send({ error: 'Invalid Updates!' })
+	}
+
+	try {
+		const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+		console.log('$$$$ task', task)
+
+		if (!task) {
+			return res.status(404).send()
+		}
+
+		res.send(task)
+	} catch (error) {
+		res.status(400).send(error)
+	}
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+	try {
+		const task = await Task.findByIdAndDelete(req.params.id)
+
+		if (!task) {
+			return res.status(404).send()
+		}
+
+		res.send(task)
+	} catch (error) {
+		res.status(400).send(error)
+	}
 })
 
 app.listen(port, () => {
