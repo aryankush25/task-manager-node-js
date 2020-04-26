f375e40fdd9ada2a284089d9594297fe7e38525e
Aryan Agarwal <aryankush025@gmail.com>
2020-04-26 19:27:05 +0530

    read code removed

--- mongodb.js
+++ mongodb.js
@@ -11,31 +11,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 	console.info('Connected to DB')
 
 	const db = client.db(databaseName)
-
-	// db.collection('users').findOne({ _id: new ObjectID('5ea581a62dc2dc578fa26de1') }, (error, user) => {
-	// 	if (error) {
-	// 		return console.error('Error - ', error)
-	// 	}
-
-	// 	console.log('User', user)
-	// })
-
-	// db.collection('users')
-	// 	.find({ age: 31 })
-	// 	.toArray((error, users) => {
-	// 		if (error) {
-	// 			return console.error('Error - ', error)
-	// 		}
-
-	// 		console.log('users', users)
-	// 	})
-
-	db.collection('tasks')
-		.find({ completed: true })
-		.toArray((error, tasks) => {
-			if (error) {
-				return console.error('Error - ', error)
-			}
-			console.log('tasks', tasks)
-		})
 })
