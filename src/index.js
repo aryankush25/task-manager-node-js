6703c56d21cf39f278871278a229356d2219288b
Aryan Agarwal <aryankush025@gmail.com>
2020-04-29 02:24:31 +0530

    added hashed password

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ app.use(taskRouter)
 app.listen(port, () => {
 	console.log('Server is up on port ' + port)
 })
+
+const bcrypt = require('bcrypt')
+
+// const myFunction = async () => {
+// 	const myPass = 'My@12345'
+// 	const hashedPassword = await bcrypt.hash(myPass, 8)
+
+// 	console.log('myPass', myPass)
+// 	console.log('hashedPassword', hashedPassword)
+
+// 	const isMatch = await bcrypt.compare('My@12345', hashedPassword)
+
+// 	console.log('isMatch', isMatch)
+// }
+
+// myFunction()
--- src/routers/task.js
+++ src/routers/task.js
@@ -49,9 +49,13 @@ router.patch('/tasks/:id', async (req, res) => {
 	}
 
 	try {
-		const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+		const task = await Task.findById(req.params.id)
 
-		console.log('$$$$ task', task)
+		updates.forEach((update) => (task[update] = req.body[update]))
+
+		task.save()
+
+		// const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }) // ------> Deprecated method middleware will not work
 
 		if (!task) {
 			return res.status(404).send()
