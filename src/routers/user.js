57d54146efcc82ec799d3f75fb64e5858deb8ffc
Aryan Agarwal <aryankush025@gmail.com>
2020-04-30 01:37:52 +0530

    update user route changed

--- src/routers/user.js
+++ src/routers/user.js
@@ -59,7 +59,7 @@ router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user)
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
 	const updates = Object.keys(req.body)
 	const allowedUpdates = ['name', 'email', 'password', 'age']
 
@@ -70,17 +70,10 @@ router.patch('/users/:id', async (req, res) => {
 	}
 
 	try {
-		const user = await User.findById(req.params.id)
+		updates.forEach((update) => (req.user[update] = req.body[update]))
+		await req.user.save()
 
-		updates.forEach((update) => (user[update] = req.body[update]))
-
-		await user.save()
-
-		if (!user) {
-			return res.status(404).send()
-		}
-
-		res.send(user)
+		res.send(req.user)
 	} catch (error) {
 		res.status(400).send(error)
 	}
