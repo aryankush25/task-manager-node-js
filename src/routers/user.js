7204c74f6df94f5862aedf2e81d27c65d8a7e6ab
Aryan Agarwal <aryankush025@gmail.com>
2020-04-30 01:01:39 +0530

    logout all devices added

--- src/routers/user.js
+++ src/routers/user.js
@@ -42,6 +42,19 @@ router.post('/users/logout', auth, async (req, res) => {
 	}
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+	try {
+		req.user.tokens = []
+		await req.user.save()
+
+		res.send()
+	} catch (error) {
+		console.log(error)
+
+		res.status(500).send(error)
+	}
+})
+
 router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user)
 })
