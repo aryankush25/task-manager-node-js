71f2da4c0bd32e0cff0744f731430f14472f443e
Aryan Agarwal <aryankush025@gmail.com>
2020-05-01 02:13:56 +0530

    tasks authentication added

--- src/models/user.js
+++ src/models/user.js
@@ -5,95 +5,101 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is invalid')
-			}
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be greater than equal to 0.')
-			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (R.includes('password', R.toLower(value))) {
-				throw new Error("Password must not contain string 'password'")
-			}
-		}
-	},
-	tokens: [
-		{
-			token: {
-				type: String,
-				require: true
-			}
-		}
-	]
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be greater than equal to 0.')
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (R.includes('password', R.toLower(value))) {
+        throw new Error("Password must not contain string 'password'")
+      }
+    },
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+})
+
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'owner',
 })
 
 userSchema.methods.toJSON = function () {
-	const user = this
-	const userObject = R.omit(['password', 'tokens'], user.toObject())
+  const user = this
+  const userObject = R.omit(['password', 'tokens'], user.toObject())
 
-	return userObject
+  return userObject
 }
 
 userSchema.methods.generateAuthToken = async function () {
-	const user = this
-	const token = jwt.sign({ _id: user._id.toString() }, 'task-manager')
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'task-manager')
 
-	user.tokens = user.tokens.concat({ token })
-	await user.save()
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
 
-	return token
+  return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
-	const user = await User.findOne({ email })
+  const user = await User.findOne({ email })
 
-	if (!user) {
-		throw new Error('Unable to login!')
-	}
+  if (!user) {
+    throw new Error('Unable to login!')
+  }
 
-	const isMatch = await bcrypt.compare(password, user.password)
+  const isMatch = await bcrypt.compare(password, user.password)
 
-	if (!isMatch) {
-		throw new Error('Unable to login!')
-	}
+  if (!isMatch) {
+    throw new Error('Unable to login!')
+  }
 
-	return user
+  return user
 }
 
 // HASH PLAIN TEXT PASSWORD
 userSchema.pre('save', async function (next) {
-	const user = this
+  const user = this
 
-	if (user.isModified('password')) {
-		user.password = await bcrypt.hash(user.password, 8)
-	}
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
 
-	next()
+  next()
 })
 
 const User = mongoose.model('User', userSchema)
