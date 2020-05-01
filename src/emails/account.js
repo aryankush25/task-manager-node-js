233a20fe57f48f95cee3f60494043cef28a7b003
Aryan Agarwal <aryankush025@gmail.com>
2020-05-02 03:57:46 +0530

    sendgrid added to send mail on cancelation

--- src/emails/account.js
+++ src/emails/account.js
@@ -10,7 +10,24 @@ const sendWelcomeEmail = (email, name) => {
       to: email,
       from: 'aryan@geekyants.com',
       subject: 'Thanks for joining in!',
-      text: `Welcome ${name}, Let me know how's the app ?`,
+      text: `Welcome ${name}, Let me know how's the app?`,
+      html: `<h1>Welcome ${name}, Let me know how's the app?</h1>`,
+    })
+    .then((res) => {
+      console.log('res', res)
+    })
+    .catch((error) => {
+      console.error('error', error)
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+  sgMail
+    .send({
+      to: email,
+      from: 'aryan@geekyants.com',
+      subject: 'Thanks for being with us!',
+      text: `Hey ${name}, Let me if there's any thing I can do to stop you?`,
     })
     .then((res) => {
       console.log('res', res)
@@ -22,4 +39,5 @@ const sendWelcomeEmail = (email, name) => {
 
 module.exports = {
   sendWelcomeEmail,
+  sendCancelationEmail,
 }
